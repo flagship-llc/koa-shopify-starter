208f3853ee3aba408dc8ead6af596cb23e168920
kabirvirji <kabirvirji@gmail.com>
2019-01-18 18:45:13 +0900

    updating VIP number and email correctly

--- server/functions.js
+++ server/functions.js
@@ -3,23 +3,27 @@ const mongoose = require('mongoose');
 require("./models/groupB");
 const vipB = mongoose.model("vipB");
 
-const generateVIPNumber = () => { 
-  // get last VIP number from database
-  // return last VIP number in database + 1
-  // add the new one in, along with the customer email
-  vipB.find(function (err, customer) {
-    if (err) return console.error(err);
-    console.log("customer", customer);
-  })
-  // return 3
-}
-
 const addVIPcustomer = async (VIPnumber, email) => {
   const newCustomer = await new vipB({
     vip_number: VIPnumber,
     customer_email: email
   }).save()
 }
+
+const generateVIPNumber = (email) => { 
+  // get last VIP number from database
+  // return last VIP number in database + 1
+  // add the new one in, along with the customer email
+  vipB.find(function (err, customer) {
+    if (err) return console.error(err);
+    console.log("customer database", customer);
+    const lastVIPcustomer = customer.pop()
+    addVIPcustomer(lastVIPcustomer.vip_number + 1, email)
+  })
+  // return 3
+}
+
+
 
 module.exports = {
 
@@ -68,8 +72,8 @@ module.exports = {
               headers: { 'X-Shopify-Access-Token': accessToken }
             }).then(function (message) {
               console.log("customer tagged as VIP"); // notify customer
-              // addVIPcustomer()
-              generateVIPNumber()
+              generateVIPNumber(customerEmail)
+              
             }).catch(function (message) {
               console.log("Error:", message);
             });
