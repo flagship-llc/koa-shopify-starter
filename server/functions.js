5262604513973049cdb27cae3589171b122ea698
kabirvirji <kabirvirji@gmail.com>
2019-01-25 01:53:51 +0900

    working metafield put request

--- server/functions.js
+++ server/functions.js
@@ -3,14 +3,61 @@ const mongoose = require('mongoose');
 require("./models/groupB");
 const vipB = mongoose.model("vipB");
 
-const addVIPcustomer = async (VIPnumber, email) => {
+// remember, to update a customer metafield you need to delete what is already there
+
+// maybe it has to be a post if no metafields exit 
+
+const updateMetafield = (VIPnumber, customer_id, shop) => {
+  const metafieldData = {
+    "customer": {
+        "id": 1000942534756,
+        "metafields": [
+          {
+            "key": "vip_nuer",
+            "value": 100,
+            "value_type": "integer",
+            "namespace": "global"
+          }
+        ]
+      }
+    }  
+  
+  console.log("updateMetafield", `https://${shop}/admin/customers/1000942534756.json`)
+  // axios({
+  //   method: 'put',
+  //   url: `https://${shop}/admin/customers/${customer_id}.json`,
+  //   data: metafieldData,
+  //   headers: { 'X-Shopify-Access-Token': "84e4a50103442b40ebb7b232548bd925" }
+  // }).then(function (message) {
+  //   console.log("customer metafield updated", message)
+  // }).catch(function (message) {
+  //   console.log("Error:", message);
+  // });
+  axios.put(
+    `https://${shop}/admin/customers/${customer_id}.json`,
+    metafieldData,
+    {
+      headers: {
+      'X-Shopify-Access-Token': "84e4a50103442b40ebb7b232548bd925",
+      'Content-Type': 'application/json'
+      }
+    }
+  ).then(response => console.log(response))
+  .catch(err => console.log(err))
+}
+
+const addVIPcustomer = async (VIPnumber, email, customer_id, shop) => {
   const newCustomer = await new vipB({
     vip_number: VIPnumber,
     customer_email: email
   }).save()
+  // update their metafield
+  updateMetafield(VIPnumber, customer_id, shop)
+
+
 }
 
-const generateVIPNumber = (email) => { 
+const generateVIPNumber = (email, customer_id, shop) => { 
   // get last VIP number from database
   // return last VIP number in database + 1
   // add the new one in, along with the customer email
@@ -18,7 +65,7 @@ const generateVIPNumber = (email) => {
     if (err) return console.error(err);
     console.log("customer database", customer);
     const lastVIPcustomer = customer.pop()
-    addVIPcustomer(lastVIPcustomer.vip_number + 1, email)
+    addVIPcustomer(lastVIPcustomer.vip_number + 1, email, customer_id, shop)
   })
   // return 3
 }
@@ -44,6 +91,9 @@ module.exports = {
       const customerEmail = customer.email
       console.log(customerEmail)
       
+      // the webhook test still triggers this because CUSTOMER is from the webhook so the VIP doesn't exist 
+      // however for the id we are using, the VIP tag does exist for that customer 
+
       if (!customer.tags.includes("VIP")) { // if customer is not VIP
         // get all orders of this customer
         const getURL = `https://${shop}/admin/customers/${customer_id}/orders.json`
@@ -72,7 +122,7 @@ module.exports = {
               headers: { 'X-Shopify-Access-Token': accessToken }
             }).then(function (message) {
               console.log("customer tagged as VIP"); // notify customer
-              generateVIPNumber(customerEmail)
+              generateVIPNumber(customerEmail, customer_id, shop)
               
             }).catch(function (message) {
               console.log("Error:", message);
