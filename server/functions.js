2b85b7d74404e2f89af48d5ee27401c6b91fceff
kabirvirji <kabirvirji@gmail.com>
2019-01-25 19:55:23 +0900

    retroactive delete and update metafields

--- server/functions.js
+++ server/functions.js
@@ -3,18 +3,126 @@ const mongoose = require('mongoose');
 require("./models/groupB");
 const vipB = mongoose.model("vipB");
 
+/*
+
+        {
+            "id": 5779025297508,
+            "namespace": "global",
+            "key": "vip_nuer",
+            "value": 100,
+            "value_type": "integer",
+            "description": null,
+            "owner_id": 1000994996324,
+            "created_at": "2019-01-24T11:53:18-05:00",
+            "updated_at": "2019-01-24T11:53:18-05:00",
+            "owner_resource": "customer",
+            "admin_graphql_api_id": "gid://shopify/Metafield/5779025297508"
+        },
+
+*/
+
 // remember, to update a customer metafield you need to delete what is already there
 
 // maybe it has to be a post if no metafields exit 
 
-const updateMetafield = (VIPnumber, customer_id, shop) => {
+// metaKey (either vip_number or total_spent)
+
+const deleteMetafield = (id, customer_id, shop) => {
+  const deleteURL = `https://${shop}/admin/customers/${customer_id}/metafields/${id}.json`
+  axios({ // need to wait so it deletes first
+    method: 'DELETE',
+    url: deleteURL,
+    headers: {'X-Shopify-Access-Token': "84e4a50103442b40ebb7b232548bd925"}
+  })
+  .then(message => console.log(message))
+  .catch(err => console.log(err))
+}
+
+
+
+const updateMetafieldTotalSpent = async (totalSpent, customer_id, shop) => {
   const metafieldData = {
     "customer": {
-        "id": 1000942534756,
+        "id": customer_id,
         "metafields": [
           {
-            "key": "vip_nuer",
-            "value": 100,
+            "key": "total_spent",
+            "value": totalSpent,
+            "value_type": "integer",
+            "namespace": "global"
+          }
+        ]
+      }
+    }
+  const URL = `https://${shop}/admin/customers/${customer_id}/metafields.json`
+  const config = {
+    headers: {
+      'X-Shopify-Access-Token': "84e4a50103442b40ebb7b232548bd925",
+    }
+  }
+  axios.get(URL, config)
+  .then(async function (response) {
+    console.log(response.data.metafields)
+    const metafields = response.data.metafields
+    // a customer can have two metafields 
+    // look at all metafields and see if they have "vip_number" or "total_spent" 
+    // store the id of the metafield 
+    // vip number need to be updated, just total_spent
+    if (metafields.length !== 0) { // if there is a metafield need to delete it
+      // if totalSpent function, then not VIP so don't need to worry about multiple metafields to delete
+      const metafieldFilter = metafields.filter(customer => customer.key === "total_spent")
+      const idToDelete = metafieldFilter.id
+      // this is undefined if it is then don't need to delete 
+      console.log("id to delete", idToDelete)
+      // delete metafield 
+      if (idToDelete !== undefined) {
+        deleteMetafield(idToDelete, customer_id, shop)
+      } else {
+        // put request to add metafield 
+        const metafieldData = {
+          "customer": {
+              "id": customer_id,
+              "metafields": [
+                {
+                  "key": "total_spent",
+                  "value": totalSpent,
+                  "value_type": "integer",
+                  "namespace": "global"
+                }
+              ]
+            }
+          }
+        // check if metafield exists 
+        axios.put(
+          `https://${shop}/admin/customers/${customer_id}.json`,
+          metafieldData,
+          {
+            headers: {
+            'X-Shopify-Access-Token': "84e4a50103442b40ebb7b232548bd925",
+            'Content-Type': 'application/json'
+            }
+          }
+        ).then(response => console.log(response))
+        .catch(err => console.log(err))
+      }
+      
+
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+}
+
+const updateMetafieldVIP = (VIPnumber, customer_id, shop) => {
+  const metafieldData = {
+    "customer": {
+        "id": customer_id,
+        "metafields": [
+          {
+            "key": "vip_number",
+            "value": VIPnumber,
             "value_type": "integer",
             "namespace": "global"
           }
@@ -22,17 +130,8 @@ const updateMetafield = (VIPnumber, customer_id, shop) => {
       }
     }  
   
-  console.log("updateMetafield", `https://${shop}/admin/customers/1000942534756.json`)
-  // axios({
-  //   method: 'put',
-  //   url: `https://${shop}/admin/customers/${customer_id}.json`,
-  //   data: metafieldData,
-  //   headers: { 'X-Shopify-Access-Token': "84e4a50103442b40ebb7b232548bd925" }
-  // }).then(function (message) {
-  //   console.log("customer metafield updated", message)
-  // }).catch(function (message) {
-  //   console.log("Error:", message);
-  // });
+
+  // check if metafield exists 
   axios.put(
     `https://${shop}/admin/customers/${customer_id}.json`,
     metafieldData,
@@ -52,7 +151,7 @@ const addVIPcustomer = async (VIPnumber, email, customer_id, shop) => {
     customer_email: email
   }).save()
   // update their metafield
-  updateMetafield(VIPnumber, customer_id, shop)
+  updateMetafieldVIP(VIPnumber, customer_id, shop)
 
 
 }
@@ -107,7 +206,7 @@ module.exports = {
             console.log(totalSpent)
           }
           // change to 5000 for prod
-          if (totalSpent > 5) { // tag the customer as VIP
+          if (totalSpent > 500000000) { // tag the customer as VIP
             console.log("total spent")
             const customerVIP = {
               "customer": {
@@ -127,6 +226,8 @@ module.exports = {
             }).catch(function (message) {
               console.log("Error:", message);
             });
+          } else { // does not meet totalSpent requirments
+            updateMetafieldTotalSpent(totalSpent, customer_id, shop)
           }
           }).catch( (error) => {
             console.log(error)
